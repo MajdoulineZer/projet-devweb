3d017ff653eb7598fdf928fe857c6eb6122967eb
MajdoulineZer <zerhounimajdouline18@gmail.com>
2022-11-29 13:41:07 +0100

    projet

---
+++ projet/mini projet.js
@@ -0,0 +1,6 @@
+<script src="https://kit.fontawesome.com/9044f3b38c.js" crossorigin="anonymous"></script>
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+btn1.onclick = () => document.getElementsByClassName("card").style.visibility='visible'
